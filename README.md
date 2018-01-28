1fe6d048a5db73e67e866ad6667d51515fb52240
tsq <1530234656@qq.com>
2018-01-29 00:17:00 +0800

    Add: reverse string

---
+++ README.md
@@ -0,0 +1,21 @@
+# About
+mastering data structures and algorithms using javascript es6
+
+### Check 
+
+```
+npm install && npm test 
+```
+
+### List
+
+* [reverse string](./reversestring/index.js)
+
+### Debug
+
+1. Add a 'debugger' statement in your function
+2. Call the function manually
+3. At the terminal, run 'node inspect index.js'
+4. To continue execution of the file, press 'c' then 'enter'
+5. To lanch a 'repl' session, type 'repl' then 'enter'
+6. To exit the 'repl', press Control + C
---
+++ reversestring/index.js
@@ -0,0 +1,45 @@
+// --- Directions
+// Given a string, return a new string with the reversed order of characters
+// --- Examples
+// reverse('apple') === 'alppa'
+// reverse('hello') === 'ollel'
+// reverse('Greetings!') === '!sgniteerG'
+
+/**
+ * solution3
+ * step: use reduce
+ */
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => character + reversed, '');
+}
+
+module.exports = reverse;
+
+
+/**
+ * solution1
+ * step:
+ * 1. turn 'str' into an array
+ * 2. call 'reverse' method on the array
+ * 3. join the array back into a string
+ * 4. return the result
+ */
+// function reverse(str) {
+//   return str.split('').reverse().join('');
+// }
+
+/**
+ * solution2
+ * step:
+ * 1. create an empty string called 'reversed'
+ * 2. for each character in the provided string
+ *    take the character and add it to the start of 'reversed'
+ * 3. return the variable 'reversed'
+ */
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }

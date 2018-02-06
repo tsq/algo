04c85372efa525b4c99251c38f2c6272261bee5d
tsq <1530234656@qq.com>
2018-02-06 10:58:20 +0800

    Add: linked list size

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -10,7 +10,27 @@ class Node {
 }
 
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+  
+  getFirst() {
+    return this.head;
+  }
 }
 
 module.exports = { Node, LinkedList };

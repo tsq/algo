2be4f4963f36983eaa6115652f270a3df649b0fb
tsq <1530234656@qq.com>
2018-02-06 11:07:23 +0800

    Add: linked list getLast()

--- linkedlist/index.js
+++ linkedlist/index.js
@@ -31,6 +31,23 @@ class LinkedList {
   getFirst() {
     return this.head;
   }
+
+  getLast() {
+    if (!this.head) {
+      return null;
+    }
+    let node = this.head;
+    while (node) {
+      if (node.next === null) {
+        return node;
+      }
+      node = node.next;
+    }
+  }
+
+  clear() {
+    this.head = null;
+  }
 }
 
 module.exports = { Node, LinkedList };
